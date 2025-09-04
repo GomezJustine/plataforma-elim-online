1a5a59f0de31d65856ed290c36f80706f4d34831
Yeison Pencue <168775486+JeisonP95@users.noreply.github.com>
2025-09-04 16:47:55 -0500

    feat(project): restructure frontend and setup backend base

---
+++ Frontend/src/pages/login/login.js
@@ -0,0 +1,47 @@
+// LOGIN (login.html)
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("loginForm");
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault(); // Evita recargar la página
+
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+    //Validación frontend
+    if (!email || !password) {
+      alert("Por favor completa todos los campos");
+      return;
+    }
+    if (!email.includes("@")) {
+      alert("Ingresa un correo válido");
+      return;
+    }
+    if (password.length < 6) {
+      alert("La contraseña debe tener al menos 6 caracteres");
+      return;
+    }
+    try {
+      //Enviar datos al backend
+      const response = await fetch("/api/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+      const result = await response.json();
+      if (!response.ok) {
+        //error desde backend
+        alert(`Error: ${result.message || "Credenciales inválidas"}`);
+        return;
+      }
+      //Si el login fue correcto
+      alert("✅ Inicio de sesión exitoso, redirigiendo...");
+      // Guardar token en localStorage (para futuras peticiones)
+      localStorage.setItem("token", result.token);
+      // Redirigir al dashboard
+      window.location.href = "/Frontend/src/pages/Dashboard/Dashboard.html";
+    } catch (err) {
+      console.error("Error de conexión:", err);
+      alert("⚠️ No se pudo conectar con el servidor, intenta más tarde");
+    }
+  });
+});
